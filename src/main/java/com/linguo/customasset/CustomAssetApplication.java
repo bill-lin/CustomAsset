73d58b8bde134388a8b5438731c25d5c52cfc15a
Bin Lin <cold.lin@gmail.com>
2016-10-03 07:20:25 +0100

    reduce memory usage

--- src/main/java/com/linguo/customasset/CustomAssetApplication.java
+++ src/main/java/com/linguo/customasset/CustomAssetApplication.java
@@ -3,11 +3,10 @@ package com.linguo.customasset;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableMongoAuditing
-@EnableSwagger2
+//@EnableSwagger2
 public class CustomAssetApplication {
 
 	public static void main(String[] args) {
